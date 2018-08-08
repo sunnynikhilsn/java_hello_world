40c44dbbbc95d14d1d7012fe05ef4e4c141733c9
Daniele Campione <nonametekno@gmail.com>
2018-08-08 11:16:03 +0200

    Added a dialog to show the output
    Added an Information Dialog to show the output.

--- src/main/java/com/campione_tech/demo/Main.java
+++ src/main/java/com/campione_tech/demo/Main.java
@@ -4,28 +4,42 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
+
+/**
+ * 
+ * 
+ * @author D. Campione
+ *
+ */
 public class Main extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
+
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText("Look, an Information Dialog");
+                alert.setContentText("Hello World! This is my first JavaFX application.");
+
+                alert.showAndWait();
             }
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
